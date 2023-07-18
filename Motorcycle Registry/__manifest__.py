4791a8fda59e0bcf8c9fe3d626c6203fb92388c7
jopi-odoo <jopi@odoo.com>
2023-07-18 16:00:26 -0600

    First commit, __manifest__ and __init__ files created.

---
+++ Motorcycle Registry/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': ''''Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+        ''',
+    'author': 'jopi-odoo',
+    'website': 'https://github.com/jopi-odoo/technical_training',
+    'application': True,
+}
