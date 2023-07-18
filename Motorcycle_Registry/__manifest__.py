874ab8a4a485db66ac9a16d65ba00a9fdb4b7999
jopi-odoo <jopi@odoo.com>
2023-07-18 17:13:57 -0600

    Corrección de archivo __manifest__ e icono de la aplicación.

--- Motorcycle_Registry/__manifest__.py
+++ Motorcycle_Registry/__manifest__.py
@@ -5,6 +5,7 @@
         ====================
         This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
         ''',
+    'category': 'Kawiil/Kawiil',
     'author': 'jopi-odoo',
     'website': 'https://github.com/jopi-odoo/technical_training',
     'application': True,
