2a9e525b67be039fa812d6d93fac5f2653ddc5b5
jopi-odoo <jopi@odoo.com>
2023-07-19 09:13:02 -0600

    [ADD] New Module

---
+++ Motorcycle_Registry/models/motorcycle_registry.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class MotorcycleRegistry(models.Model):
+    _name = 'motorcycle.registry'
+    _description = 'Motorcycle description'
+    _rec_name = 'registry_number'
+
+    registry_number = fields.Char(required=True)
+    vin = fields.Char(required=True)
+    first_name = fields.Char(required=True)
+    last_name = fields.Char(required=True)
+    picture = fields.Image()
+    current_mileage = fields.Float()
+    license_plate = fields.Char()
+    certificate_title = fields.Binary()
+    register_date = fields.Date()
