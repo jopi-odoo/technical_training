7d33eaa1b1b9aadae769571e21842108aa7e8a18
jopi-odoo <jopi@odoo.com>
2023-07-19 10:13:25 -0600

    [ADD] Demo Data

--- Motorcycle_Registry/__manifest__.py
+++ Motorcycle_Registry/__manifest__.py
@@ -5,8 +5,13 @@
         ====================
         This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
         ''',
-    'category': 'Kawiil/Kawiil',
     'author': 'jopi-odoo',
     'website': 'https://github.com/jopi-odoo/technical_training',
+    'category': 'Kawiil/Kawiil',
+    'depends': ['base',],
+    'data': [],
+    'demo': [
+        'demo/registry_demo.xml', 
+    ],
     'application': True,
 }
