9ac6897f14f266f8c4f71e850f14450ad6055fd9
jopi-odoo <jopi@odoo.com>
2023-07-20 12:02:02 -0600

    [ADD] Advanced Fields

--- Motorcycle_Registry/models/motorcycle_registry.py
+++ Motorcycle_Registry/models/motorcycle_registry.py
@@ -7,6 +7,8 @@ class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle description'
     _rec_name = 'registry_number'
+    _sql_constraints = [('unique_vin', 'unique(vin)', 'The VIN must be unique.'),]
+    
 
     registry_number = fields.Char(required=True,
                                     string="Registry Number",
@@ -14,16 +16,20 @@ class MotorcycleRegistry(models.Model):
                                     copy=False,
                                     readonly=True)
     vin = fields.Char(required=True)
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
     picture = fields.Image()
     current_mileage = fields.Float()
     license_plate = fields.Char()
     certificate_title = fields.Binary()
     register_date = fields.Date()
 
+    make = fields.Char(string='Brand', compute='_compute_from_vin')
+    model = fields.Char(string='Make', compute='_compute_from_vin')
+    year = fields.Char(string='Model', compute='_compute_from_vin')
 
-    _sql_constraints = [('unique_vin', 'unique(vin)', 'The VIN must be unique.'),]
+
+    owner_id = fields.Many2one(comodel_name='res.partner', string = 'Owner', ondelete='cascade', required=True, default=None)
+    phone = fields.Char(related='owner_id.phone')
+    email = fields.Char(related='owner_id.email')
 
 
     @api.model_create_multi
@@ -56,4 +62,16 @@ class MotorcycleRegistry(models.Model):
                                       1-4 Capital Letters \n
                                       1-3 Digits \n
                                       Optional 2 Capital Letters """)
+    
+
+    @api.depends('vin')
+    def _compute_from_vin(self):
+        for record in self:
+            if record.vin:
+                record.make = record.vin[:2]
+                record.model = record.vin[2:4]
+                record.year = record.vin[4:6]
+            else:
+                record.make =  record.model = record.year = ''
+
             
