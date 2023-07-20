d18cf0b66ab8f6866bf1fcdd3fc14e4dc945baf8
jopi-odoo <jopi@odoo.com>
2023-07-20 09:41:50 -0600

    [ADD] Advanced ORM

--- Motorcycle_Registry/models/motorcycle_registry.py
+++ Motorcycle_Registry/models/motorcycle_registry.py
@@ -1,4 +1,7 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+import re
 
 class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
@@ -19,8 +22,38 @@ class MotorcycleRegistry(models.Model):
     certificate_title = fields.Binary()
     register_date = fields.Date()
 
-    @api.model
-    def created(self, vals):
-        if vals.get('registry_number', ('MRN0000')) == ('MRN0000'):
-            vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
-        return super().create(vals)
+
+    _sql_constraints = [('unique_vin', 'unique(vin)', 'The VIN must be unique.'),]
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('registry_number', ('MRN0000')) == ('MRN0000'):
+                vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
+        return super().create(vals_list)
+
+
+    @api.constrains('vin')
+    def _check_vin(self):
+        vin_pattern = r'^[A-Z]{4}\d{2}[A-Z\d]{2}\d{6}$'
+        for registry_number in self:
+            if re.match(vin_pattern, registry_number.vin) == None:
+                raise ValidationError("""The VIN must follow the pattern \n
+                                      Make - 2 Capital Letters \n
+                                      Model - 2 Capital Letters \n
+                                      Year - 2 Digits \n
+                                      Battery Capacity - 2 Capital Letters or Numbers \n
+                                      Serial Number - 6 Digits """)
+
+    
+    @api.constrains('license_plate')
+    def _check_license_plate(self):
+        license_plate_pattern = r'^[A-Z]{1,4}\d{1,3}(?:[A-Z]{2})?$'
+        for registry_number in self:
+            if re.match(license_plate_pattern, registry_number.license_plate) == None:
+                raise ValidationError("""The License Plate must follow the pattern \n
+                                      1-4 Capital Letters \n
+                                      1-3 Digits \n
+                                      Optional 2 Capital Letters """)
+            
